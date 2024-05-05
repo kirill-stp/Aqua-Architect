e2359b52ee5f91019e3aaf9b9251faf073b628b8
Kirill Stepanishin <kirill.stpn.by@gmail.com>
2024-05-04 18:19:32 -0700

    Add Basic Placement

--- Aqua Architect/Assets/Scripts/PlacementSystem.cs
+++ Aqua Architect/Assets/Scripts/PlacementSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class PlacementSystem : MonoBehaviour
 {
@@ -13,8 +14,60 @@ public class PlacementSystem : MonoBehaviour
     
     [SerializeField] private InputManager inputManager;
 
+    [SerializeField] private ObjectsDatabaseSO databaseSO;
+    private int selectetObjectIndex = -1;
+
+    [SerializeField] private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectetObjectIndex = databaseSO.objectsData.FindIndex(data => data.ID == ID);
+        if (selectetObjectIndex < 0)
+        {
+            Debug.LogError($"No ID found {ID}");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.isPointerOverUI())
+        {
+            return;
+        }
+        
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        GameObject newObject = Instantiate(databaseSO.objectsData[selectetObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void StopPlacement()
+    {
+        selectetObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
     private void Update()
     {
+        if (selectetObjectIndex < 0)
+        {
+            return;
+        }
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
 
